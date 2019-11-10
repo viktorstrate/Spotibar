56e383ec677995767c5209e0d9f4ea7e46a1487d
viktorstrate <viktorstrate@gmail.com>
2019-11-10 17:35:00 +0100

    Initial Commit

---
+++ Spotibar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Spotibar
+//
+//  Created by Viktor Strate Kløvedal on 10/11/2019.
+//  Copyright © 2019 viktorstrate. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Spotibar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Spotibar
+//
+//  Created by Viktor Strate Kløvedal on 10/11/2019.
+//  Copyright © 2019 viktorstrate. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
