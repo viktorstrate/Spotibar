674714f6c1be64c00634db8d86488bd0e007b6ec
viktorstrate <viktorstrate@gmail.com>
2020-10-25 15:40:22 +0100

    reindent files

--- Spotibar/AppDelegate.swift
+++ Spotibar/AppDelegate.swift
@@ -11,110 +11,109 @@ import ScriptingBridge
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    lazy var spotify: SpotifyApplication = SBApplication(bundleIdentifier: "com.spotify.client")!
-    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.variableLength)
-    var eventMonitor: EventMonitor?
+  
+  lazy var spotify: SpotifyApplication = SBApplication(bundleIdentifier: "com.spotify.client")!
+  let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.variableLength)
+  var eventMonitor: EventMonitor?
+  
+  let popover = NSPopover()
+  var spotifyTimer: Timer?
+  
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
     
-    let popover = NSPopover()
-    var spotifyTimer: Timer?
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-
-        if let button = statusItem.button {
-            button.image = NSImage(named: NSImage.Name("StatusBarIcon"))
-            button.imagePosition = .imageLeft
-            button.attributedTitle = NSAttributedString(string: "Hello")
-            button.action = #selector(pressItem)
+    if let button = statusItem.button {
+      button.image = NSImage(named: NSImage.Name("StatusBarIcon"))
+      button.imagePosition = .imageLeft
+      button.attributedTitle = NSAttributedString(string: "Hello")
+      button.action = #selector(pressItem)
+    }
+    
+    
+    
+    eventMonitor = EventMonitor(mask: [.leftMouseDown, .rightMouseDown]) { [weak self] event in
+      if let strongSelf = self, strongSelf.popover.isShown {
+        strongSelf.closePopover(sender: event)
+      }
+    }
+    
+    periodicUpdate()
+    
+  }
+  
+  var lastUpdateArtworkUrl: String? = nil
+  
+  @objc func periodicUpdate() {
+    spotifyTimer?.invalidate()
+    
+    if self.spotify.isRunning {
+      if let button = statusItem.button {
+        
+        let topLine = "\(spotify.currentTrack!.artist!)".truncate(length: 20)
+        let bottomLine = "\(spotify.currentTrack!.name!)".truncate(length: 20)
+        
+        let sharedPS = NSMutableParagraphStyle()
+        sharedPS.alignment = .left
+        
+        
+        let titleAttributes: [NSAttributedString.Key: Any] = [
+          .font: NSFont.menuBarFont(ofSize: 10),
+          .paragraphStyle: sharedPS,
+        ]
+        
+        let lineHeightPS = NSMutableParagraphStyle()
+        lineHeightPS.maximumLineHeight = 8
+        
+        let title = NSMutableAttributedString(string: "\(topLine)\n\(bottomLine)", attributes: titleAttributes)
+        title.addAttribute(.paragraphStyle, value: lineHeightPS, range: NSRange(location: topLine.count+1, length: bottomLine.count))
+        
+        button.attributedTitle = title
+        
+        guard let newArtworkUrl = self.spotify.currentTrack!.artworkUrl else {
+          button.image = nil
+          return
         }
         
-        
-        
-        eventMonitor = EventMonitor(mask: [.leftMouseDown, .rightMouseDown]) { [weak self] event in
-          if let strongSelf = self, strongSelf.popover.isShown {
-            strongSelf.closePopover(sender: event)
+        if (newArtworkUrl != lastUpdateArtworkUrl) {
+          // Upgrade cover art url to https
+          let imageUrl = NSURLComponents(string: newArtworkUrl)!
+          imageUrl.scheme = "https"
+          
+          guard let originalImage = NSImage(contentsOf: imageUrl.url!) else {
+            return
           }
+          
+          let thumbnailImage = originalImage.asThumbnail()
+          
+          button.image = thumbnailImage
+          
+          lastUpdateArtworkUrl = newArtworkUrl
         }
-
-        periodicUpdate()
-        
+      }
+      
+      spotifyTimer = Timer.scheduledTimer(timeInterval: 4, target: self, selector: #selector(periodicUpdate), userInfo: nil, repeats: false)
+    } else {
+      if let button = self.statusItem.button {
+        button.attributedTitle = NSAttributedString(string: "")
+        button.image = NSImage(named: NSImage.Name("StatusBarIcon"))
+      }
+      
+      spotifyTimer = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(periodicUpdate), userInfo: nil, repeats: false)
     }
+  }
+  
+  func closePopover(sender: Any?) {
     
-    var lastUpdateArtworkUrl: String? = nil
-    
-    @objc func periodicUpdate() {
-        spotifyTimer?.invalidate()
-        
-        if self.spotify.isRunning {
-            if let button = statusItem.button {
-                
-                let topLine = "\(spotify.currentTrack!.artist!)".truncate(length: 20)
-                let bottomLine = "\(spotify.currentTrack!.name!)".truncate(length: 20)
-                
-                let sharedPS = NSMutableParagraphStyle()
-                sharedPS.alignment = .left
-                
-                
-                let titleAttributes: [NSAttributedString.Key: Any] = [
-                    .font: NSFont.menuBarFont(ofSize: 10),
-                    .paragraphStyle: sharedPS,
-                ]
-                
-                let lineHeightPS = NSMutableParagraphStyle()
-                lineHeightPS.maximumLineHeight = 8
-                
-                let title = NSMutableAttributedString(string: "\(topLine)\n\(bottomLine)", attributes: titleAttributes)
-                //title.addAttribute(.baselineOffset, value: -1, range: NSRange(location: 0, length: topLine.count))
-                title.addAttribute(.paragraphStyle, value: lineHeightPS, range: NSRange(location: topLine.count+1, length: bottomLine.count))
-                
-                button.attributedTitle = title
-                
-                guard let newArtworkUrl = self.spotify.currentTrack!.artworkUrl else {
-                    button.image = nil
-                    return
-                }
-                
-                if (newArtworkUrl != lastUpdateArtworkUrl) {
-                    // Upgrade cover art url to https
-                    let imageUrl = NSURLComponents(string: newArtworkUrl)!
-                    imageUrl.scheme = "https"
-                    
-                    guard let originalImage = NSImage(contentsOf: imageUrl.url!) else {
-                        return
-                    }
-                    
-                    let thumbnailImage = originalImage.asThumbnail()
-                    
-                    button.image = thumbnailImage
-                    
-                    lastUpdateArtworkUrl = newArtworkUrl
-                }
-            }
-            
-            spotifyTimer = Timer.scheduledTimer(timeInterval: 4, target: self, selector: #selector(periodicUpdate), userInfo: nil, repeats: false)
-        } else {
-            if let button = self.statusItem.button {
-                button.attributedTitle = NSAttributedString(string: "")
-                button.image = NSImage(named: NSImage.Name("StatusBarIcon"))
-            }
-            
-            spotifyTimer = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(periodicUpdate), userInfo: nil, repeats: false)
-        }
-    }
-    
-    func closePopover(sender: Any?) {
-        
-    }
-
-    @objc func pressItem(_ sender: Any) {
-        periodicUpdate()
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-
+  }
+  
+  @objc func pressItem(_ sender: Any) {
+    periodicUpdate()
+  }
+  
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+  
+  
 }
 
--- Spotibar/Extensions/NSImage.swift
+++ Spotibar/Extensions/NSImage.swift
@@ -9,33 +9,33 @@
 import Cocoa
 
 extension NSImage {
-    func asThumbnail() -> NSImage {
-        let targetSize = NSMakeSize(16, 16)
-        let img = NSImage(size: targetSize)
-
-        img.lockFocus()
-        let ctx = NSGraphicsContext.current
-        let cgCtx = ctx?.cgContext
-        
-        let targetRect = NSMakeRect(0, 0, targetSize.width, targetSize.height)
-        
-        let borderRadius: CGFloat = 2.0
-        let roundedMask = NSBezierPath(roundedRect: targetRect, xRadius: borderRadius, yRadius: borderRadius)
-        roundedMask.setClip()
-        
-        ctx?.imageInterpolation = .none
-        self.draw(in: targetRect, from: NSMakeRect(0, 0, size.width, size.height), operation: .copy, fraction: 1)
-        img.unlockFocus()
-        
-        cgCtx?.setLineWidth(0.5)
-        cgCtx?.setStrokeColor(CGColor.black)
-        
-        let borderPath = CGMutablePath()
-        borderPath.addRoundedRect(in: targetRect.insetBy(dx: 0.2, dy: 0.2), cornerWidth: borderRadius, cornerHeight: borderRadius)
-        
-        cgCtx?.addPath(borderPath)
-        cgCtx?.drawPath(using: .stroke)
-
-        return img
-    }
+  func asThumbnail() -> NSImage {
+    let targetSize = NSMakeSize(16, 16)
+    let img = NSImage(size: targetSize)
+    
+    img.lockFocus()
+    let ctx = NSGraphicsContext.current
+    let cgCtx = ctx?.cgContext
+    
+    let targetRect = NSMakeRect(0, 0, targetSize.width, targetSize.height)
+    
+    let borderRadius: CGFloat = 2.0
+    let roundedMask = NSBezierPath(roundedRect: targetRect, xRadius: borderRadius, yRadius: borderRadius)
+    roundedMask.setClip()
+    
+    ctx?.imageInterpolation = .none
+    self.draw(in: targetRect, from: NSMakeRect(0, 0, size.width, size.height), operation: .copy, fraction: 1)
+    img.unlockFocus()
+    
+    cgCtx?.setLineWidth(0.5)
+    cgCtx?.setStrokeColor(CGColor.black)
+    
+    let borderPath = CGMutablePath()
+    borderPath.addRoundedRect(in: targetRect.insetBy(dx: 0.2, dy: 0.2), cornerWidth: borderRadius, cornerHeight: borderRadius)
+    
+    cgCtx?.addPath(borderPath)
+    cgCtx?.drawPath(using: .stroke)
+    
+    return img
+  }
 }
